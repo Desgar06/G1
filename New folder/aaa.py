681c7e6ba8eaa2ffcf67992b8bc9cbeb9ea0aacb
Alejandro <noperdergranmk@gmail.com>
2024-07-11 20:06:02 -0500

    Puta

--- New folder/aaa.py
+++ New folder/aaa.py
@@ -1,4 +1,4 @@
 color = input("¿Cuál es el color de tu cerveza? ")
 tamaño = input("¿Cuál es el tamaño de tu cerveza? ")
 
-print("Tu cerveza podría llamarse " + tamaño + " " + color + "  ")
+print("Tu cerveza podría llamarse " + tamaño + " " + color )
