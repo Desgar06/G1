9ab0b92ab5a6940db8c7ab1f6b1c4ea96fb93b28
Alejandro <noperdergranmk@gmail.com>
2024-07-12 18:51:18 -0500

    operaciones

--- New folder/FormateodeStrings.py
+++ New folder/FormateodeStrings.py
@@ -1,7 +1,14 @@
-x = 10
-y = 5
+x = 874
+y = 27
+z = 783
+a = 456
+b = 33
 print("Mis numeros son {x} y {y}". format(x,y))
 
-color= "verde"
-matricula= "KYK-83"
-print(f"el auto {color} tiene la matricula {matricula}")
+# color= "verde"
+# matricula= "KYK-83"
+# print(f"el auto {color} tiene la matricula {matricula}")
+
+print(f"{x} dividido entre {y} da {x/y}")
+print(f"El modulo de {a} dividido entre {b} da {x//y}")
+print(f"La raíz cuadrada de {z} es igual a {z**0.5}")
--- New folder/Operacion.py
+++ New folder/Operacion.py
@@ -1,5 +1,6 @@
-nombre = input("Digite su nombre ")
-edad = input("Digite su edad ")
+# nombre = input("Digite su nombre ")
+# edad = input("Digite su edad ")
 
-print("Hola, " + nombre + "felicitaciones por haber sobrevivido " + edad + "años" )
+# print("Hola, " + nombre + "felicitaciones por haber sobrevivido " + edad + "años" )
+ 
  
