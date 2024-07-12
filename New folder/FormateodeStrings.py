90896ca15225345b565633bb74830b2afc3c8482
Alejandro <noperdergranmk@gmail.com>
2024-07-12 18:30:04 -0500

    Formateo de cadenas

---
+++ New folder/FormateodeStrings.py
@@ -0,0 +1,7 @@
+x = 10
+y = 5
+print("Mis numeros son {x} y {y}". format(x,y))
+
+color= "verde"
+matricula= "KYK-83"
+print(f"el auto {color} tiene la matricula {matricula}")
---
+++ New folder/Operacion.py
@@ -0,0 +1,5 @@
+nombre = input("Digite su nombre ")
+edad = input("Digite su edad ")
+
+print("Hola, " + nombre + "felicitaciones por haber sobrevivido " + edad + "años" )
+ 
