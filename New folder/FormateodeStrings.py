d33b48fe086bc3871169e6fee7f615943c8ce067
Alejandro <noperdergranmk@gmail.com>
2024-07-12 19:28:38 -0500

    Redondeo

--- New folder/FormateodeStrings.py
+++ New folder/FormateodeStrings.py
@@ -1,14 +1,24 @@
 x = 874
 y = 27
 z = 783
-a = 456
-b = 33
-print("Mis numeros son {x} y {y}". format(x,y))
+a = 5
+b = 3
+c = 10
+d = c/b
+# print(f"Mis numeros son {x} y {y}")
 
-# color= "verde"
-# matricula= "KYK-83"
-# print(f"el auto {color} tiene la matricula {matricula}")
+# # color= "verde"
+# # matricula= "KYK-83"
+# # print(f"el auto {color} tiene la matricula {matricula}")
 
-print(f"{x} dividido entre {y} da {x/y}")
-print(f"El modulo de {a} dividido entre {b} da {x//y}")
-print(f"La raíz cuadrada de {z} es igual a {z**0.5}")
+# print(f"{x} dividido entre {y} da {x/y}")
+# print(f"El modulo de {a} dividido entre {b} da {x//y}")
+# print(f"La raíz cuadrada de {z} es igual a {z**0.5}")
+
+raizCuadrada = x**0.5
+numeroRedondeado = round(raizCuadrada,2)
+print(round(10/3,2))
+
+print(round(a**0.5,4))
+
+print(round(10.676767))
