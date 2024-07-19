d0680c62120559b3f98f2bc0fac06fb1da52cae7
Alejandro <noperdergranmk@gmail.com>
2024-07-18 21:25:04 -0500

    Listas

---
+++ New folder/index.py
@@ -0,0 +1,8 @@
+print ("Repeticion " *15 )
+
+haiku = "tierra mojada \n mis recuerdos de viaje \n entre las lluvias"
+print (haiku)
+
+print ("agua" in haiku)
+
+print(len ("electroencefalografista"))
---
+++ New folder/listas.py
@@ -0,0 +1,13 @@
+miLista = ['Si' , 'no' , '7' , '8.4' , 'jaja']
+print(miLista)
+
+mediosTransporte = ['avion' , 'auto' , 'barco' , 'bicicleta']
+
+mediosTransporte.append('motocicleta')
+
+print (mediosTransporte)
+
+frutas = ['manzana' , 'banana' , 'mango' , 'cereza' , 'sandia' ]
+ejercicio =frutas.pop(2)
+
+print(frutas)
